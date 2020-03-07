45d02aa8c1b1b5338a547c1137d20bbfa454974a
hugo <hugo.bricoult0521@gmail.com>
2020-03-07 02:40:31 +0100

    header

--- pages/actualites.php
+++ pages/actualites.php
@@ -1,38 +1,38 @@
+<?php
+
+function generate_News($title,$img_Path,$img_desc,$text){
+    echo "
+    <div class='row'>";
+    if($img_Path == null){
+        echo"<div class='d-flex flex-column col-12'>";
+    }else{
+        echo"
+            <img src='$img_Path' alt='$img_desc' class='col-5'>
+            <div class='d-flex flex-column col-7'>";
+    }
+    echo"
+            <div class='d-flex flex-row'>
+                <img src='' alt=''>
+                <div class='d-flex flex-column'>
+                    <h2>$title</h2>
+                    <p>$text</p>
+                </div>
+            </div>
+        <p></p>
+        </div>
+    </div>
+    ";
+};
+
+?>
 
 <div class="container">
-    <!--titre - image - text - personne - date-->
-    <div>
-        <h2></h2>
-        <div>
-            <img src="" alt="">
-            <p></p>
-        </div>
-        <div>
-            <p></p>
-            <p></p>
-        </div>
-    </div>
-    <!--titre - image - text - personne - date-->
-    <div>
-    <h2></h2>
-        <div>
-            <p></p>
-        </div>
-        <div>
-            <p></p>
-            <p></p>
-        </div>
-    </div>
-    <!--titre - image - text - personne - date-->
-    <div>
-    <h2></h2>
-        <div>
-            <img src="" alt="">
-            <p></p>
-        </div>
-        <div>
-            <p></p>
-            <p></p>
-        </div>
-    </div>
+    <?php
+    generate_News('Bonjour la commune !','./assets/img/logo.png','','description');
+    generate_News('Bonjour la commune2 !',null,'','description1');
+    generate_News('Bonjour la commune3 !',null,'','description2');
+    ?>
 </div>
+
+
+
--- pages/home.php
+++ pages/home.php
@@ -1,4 +1,15 @@
-<div>
+<div class="d-flex justify-content-center">
+
+        <label class="">Journeaux
+            <input type="radio" name="menu" id="">
+        </label>
+        <label class="">Actualités
+            <input type="radio" name="menu" id="" checked>
+        </label>
+        <label class="">Vidéos
+            <input type="radio" name="menu" id="">
+        </label>
+
     <a href="index.php?page=home&section=journeaux">Journeaux</a>
     <a href="index.php?page=home&section=actualites">Actualités</a>
     <a href="index.php?page=home&section=videos">Vidéos</a>
@@ -7,6 +18,6 @@
 if(isset($_GET['section'])){
     include('./pages/'.$_GET['section'].'.php');
 }else{
-    include('./pages/journeaux.php');
+    include('./pages/actualites.php');
 }
 ?>
