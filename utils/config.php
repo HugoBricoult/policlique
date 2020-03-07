568bbd370cdf990b88b740b409250c5a22e5f067
hugo <hugo.bricoult0521@gmail.com>
2020-03-07 08:30:15 +0100

    config

--- utils/config.php
+++ utils/config.php
@@ -1,3 +1,12 @@
 <?php
 $bdd = new PDO ("mysql:host=localhost; dbname=test; charset=utf8','root, '',");
+//setup nouveau server
+//
+/* CREATE TABLE `test`.`actualites` (
+    `ID` INT NOT NULL AUTO_INCREMENT,
+    `sujet` VARCHAR(64) NOT NULL ,
+    `votespos` INT,
+    `votesneg` INT,
+    `auteur_ID` INT,
+    PRIMARY KEY (`ID`)) ENGINE = InnoDB; */
 ?>
