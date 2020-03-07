73a24e68ce10ff1db37ce859766c7c66f57d5da2
hugo <hugo.bricoult0521@gmail.com>
2020-03-07 01:47:01 +0100

    save

--- header.php
+++ header.php
@@ -1,14 +1,19 @@
 <nav>
-    <ul>
+    <ul class="nav-mobile">
         <li><a href=""><i class="material-icons">home</i></a></li>
         <li><a href=""><i class="material-icons">how_to_vote</i></a></li>
         <li><a href=""><i class="material-icons">person</i></a></li>
         <li><a href=""><i class="material-icons">location_city</i></a></li>
     </ul>
-    <ul>
-        <li><a href="">Accueil</a></li>
-        <li><a href="">Votes</a></li>
-        <li><a href="">Profil</a></li>
-        <li><a href="">Commune</a></li>
+    <ul class="nav-desktop">
+        <div>
+            <h1>COMMUNE</h1>
+        </div>
+        <div>
+            <li><a href="">Accueil</a></li>
+            <li><a href="">Votes</a></li>
+            <li><a href="">Profil</a></li>
+            <li><a href="">Commune</a></li>
+        </div>
     </ul>
 </nav>
