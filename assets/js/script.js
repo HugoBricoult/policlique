fbb71291f343448a25495520148865472e594061
Brice Levizar Bartoletti <bartolettibrice@gmail.com>
2020-03-08 06:37:17 +0100

    need to fix AI api call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
-const data = document.getElementById('text').value;
+
 const aiAnalyze = async() => {
-    const rep = await fetch('http://localhost:1880/text',{
+    let data = document.getElementById('text').value;
+    console.log(data)
+    // L'erreur vient surement de la communication entre JS et NodeRed
+    // Il faut surement parser le text en JSON avant de l'envoyer (ce que je n'ai pas fait)
+    // data = {data}
+    const rep = await fetch('http://0.0.0.0:1880/text',{
         method : "POST",
         body: data
     });
